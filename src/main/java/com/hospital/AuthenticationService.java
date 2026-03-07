175398bce930444ac96740032eaf6e9b1d9df600
HETPATEL2008 <hetp5478@gmail.com>
2026-03-07 21:55:02 +0530

    feat: Add AuthenticationService.java

---
+++ src/main/java/com/hospital/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.hospital;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuthenticationService {
+
+    // Logger for tracking authentication services
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
+}
